997dd795d699ec4c78cd94857e20ab4dfe315fd2
pcyang1230 <71486223+pcyang1230@users.noreply.github.com>
2020-12-18 15:02:04 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
+let soundModelURL = 'https://pcyang1230.github.io/aifrog/';
 
 
 function preload() {
